0fd55e4d8572c1c2d5c873b906746ca6a2c95719
105062270 <105062270@student.swin.edu.au>
2026-02-25 18:30:34 +1100

    add css,bootstrap and js structure for project

--- src/main/java/com/kaiet/laptopshop/controller/UserController.java
+++ src/main/java/com/kaiet/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.kaiet.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,10 @@ public class UserController {
   }
 
   @RequestMapping("/")
-  public String getHomePage(){
+  public String getHomePage(Model model){
     String test = service.handleHello();
-    return "kaiet.html";
+    model.addAttribute("kaiet",test);
+    model.addAttribute("hello","Hello from controller");
+    return "hello";
   }
 }
