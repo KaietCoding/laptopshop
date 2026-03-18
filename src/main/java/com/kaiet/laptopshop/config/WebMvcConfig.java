1c9e2b7ebc91fe5e7bcbbca1cb2b750172e98c6b
105062270 <105062270@student.swin.edu.au>
2026-03-19 00:03:36 +1100

    add admin-dashboard template from bootstrap template

--- src/main/java/com/kaiet/laptopshop/config/WebMvcConfig.java
+++ src/main/java/com/kaiet/laptopshop/config/WebMvcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-  registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+    registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+    registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
   }
 }
